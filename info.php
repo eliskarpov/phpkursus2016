618a72f218995e5b7c4b5427953f10bc1e25b208
eliskarpov <eliskarpov@gmail.com>
2016-05-16 12:40:41 +0300

    Update info.php

--- info.php
+++ info.php
@@ -5,7 +5,7 @@
 2. Mida ütleb su sünnikuu sinu isiksuse kohta<br>
 3. Millised lood olid sinu sünniaastal UK top 40-s esimesel kohal<br>
 <p>
-Vastus kuvatakse, kui valida huvipakkuva küsimuse all asuvast rippmenüüst nõutud sünniaeg.<br>
+Vastus kuvatakse, kui valida huvipakkuva küsimuse all asuvast rippmenüüst oma sünniaeg.<br>
 Küsimused ja vastused on inglise keeles.<br>
 <p>
 <b>Autorist ja programmist</b><br>
