04b71d033fc10e90a3c2916b738329b5798439c8
eliskarpov <eliskarpov@gmail.com>
2016-05-16 19:03:05 +0300

    Add files via upload

---
+++ info.php
@@ -0,0 +1,15 @@
+<h2>Programmi "Mida ütleb su sünnipäev" infofail</h2>
+
+<b>Programmi kaudu saab teada:</b><br>
+1. Mida ütleb su sünnikuupäev su karjääri kohta<br>
+2. Mida ütleb su sünnikuu sinu isiksuse kohta<br>
+3. Millised lood olid sinu sünniaastal UK top 40-s esimesel kohal<br>
+<p>
+Vastus kuvatakse, kui valida huvipakkuva küsimuse all asuvast rippmenüüst oma sünniaeg.<br>
+Küsimused ja vastused on inglise keeles.<br>
+<p>
+<b>Autorist ja programmist</b><br>
+Käivitamine:<br>
+Autor: Elis Karpov<br>
+Rühm: PHP kursus 2016<br>
+Juhendaja: A. Klooster<br>
